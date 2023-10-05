0b7c0ebd7a97df1a861f81593cddfc8d316786df
Pratik Phadtare <54789620+TheResilient@users.noreply.github.com>
2023-10-05 16:48:32 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ViteSSG } from 'vite-ssg'
 import { routes } from './router'
 import App from './App.vue'
-
 import './styles/tailwind.css'
 
-export const createApp = ViteSSG(App, { routes })
+export const createApp = ViteSSG(App, { router })
